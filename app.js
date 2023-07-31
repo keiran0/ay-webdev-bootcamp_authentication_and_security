1f175b60c0a88445e262e779226c6725ec3caff3
Keiran <keinn2210@gmail.com>
2023-07-31 23:25:44 +0800

    Level 2 - Encryption with mongoose-encryption

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
 
 db = mongoose.connect("mongodb://127.0.0.1:27017/userDB")
 
@@ -20,12 +21,17 @@ app.get("/register", function(req, res){
     res.render("register")
 })
 
-//Level 1 authentication - Using DB of users.
+//Level 2 authentication - Encryption with mongoose-encryption
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
+
+const secret = "Thisisourlittlesecret." //still pretty dangerous. app.js may be revealed, not that difficult to find. But better than no encryption.
+
+//Important to add this plugin to the schema before you create the mongoose model.
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']}) //encrypt entire database without the encryptedFields key. You may not want this, so it is changed to encrypt only the password. It will encrypt when you call 'save', and decrypt when you call 'find'
 
 const User = mongoose.model("User", userSchema);
 
@@ -33,7 +39,7 @@ app.post("/register", function(req,res){
 
     newUser = new User({
         email: req.body.username,
-        password: req.body.password //it's being stored in plain text! :(
+        password: req.body.password
     })
 
     newUser.save()
